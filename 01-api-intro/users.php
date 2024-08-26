8cf900b8b57d452eb8f571c5108d51c88174cb16
Lucas Delobelle <lucas@ld-web.net>
2024-08-26 12:22:29 +0200

    :sparkles: API Intro, JS Client

---
+++ 01-api-intro/users.php
@@ -0,0 +1,44 @@
+<?php
+
+$users = [
+    [
+        "name" => "Wright",
+        "firstname" => "Warren",
+        "email" => "peltumon@wo.ne",
+    ],
+    [
+        "name" => "Cruz",
+        "firstname" => "Grace",
+        "email" => "holbisun@lo.nc",
+    ],
+    [
+        "name" => "Santiago",
+        "firstname" => "Beulah",
+        "email" => "ijo@ogesade.do",
+    ],
+    [
+        "name" => "Day",
+        "firstname" => "Wayne",
+        "email" => "hos@rac.pt",
+    ],
+    [
+        "name" => "Wilkerson",
+        "firstname" => "Clayton",
+        "email" => "suiva@vegecza.pr",
+    ],
+    [
+        "name" => "Moss",
+        "firstname" => "Harvey",
+        "email" => "jamo@poccucal.sm",
+    ],
+    [
+        "name" => "Ballard",
+        "firstname" => "Dean",
+        "email" => "gidi@tav.nz",
+    ],
+    [
+        "name" => "Beck",
+        "firstname" => "Oscar",
+        "email" => "rogih@iw.ba",
+    ],
+];
