ab013794e7a57ee867e4d3aa64f3fcc185c89a5f
Lucas Delobelle <lucas@ld-web.net>
2024-08-27 10:08:03 +0200

    :card_file_box: Moved PDO creation
    to functions

--- 04-rest-api/index.php
+++ 04-rest-api/index.php
@@ -1,22 +1,7 @@
 <?php
 
+require_once 'functions/db.php';
+
 header('Content-Type: application/json; charset=UTF-8');
 
-[
-    'DB_HOST' => $host,
-    'DB_PORT' => $port,
-    'DB_NAME' => $dbName,
-    'DB_USER' => $dbUser,
-    'DB_PASSWORD' => $dbPassword,
-    'DB_CHARSET' => $dbCharset
-] = parse_ini_file(__DIR__ . '/db.ini');
-
-$dsn = "mysql:host=$host;port=$port;dbname=$dbName;charset=$dbCharset";
-
-try {
-    $pdo = new PDO($dsn, $dbUser, $dbPassword);
-} catch (PDOException $e) {
-    http_response_code(500);
-    echo json_encode(['error' => 'Erreur lors de la connexion à la base de données']);
-    exit;
-}
+$pdo = getConnection();
